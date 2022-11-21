9d95646b7d58039279f9c5e8f57c6774058bd327
vitor.rodrigues <vitor.rodrigues>
2022-11-21 20:08:42 +0000

    implementação de jwt

--- src/main/java/io/sld/riskcomplianceloginservice/config/SecurityConfiguration.java
+++ src/main/java/io/sld/riskcomplianceloginservice/config/SecurityConfiguration.java
@@ -1,12 +1,20 @@
 package io.sld.riskcomplianceloginservice.config;
 
+import io.sld.riskcomplianceloginservice.config.filter.JwtFilter;
+import io.sld.riskcomplianceloginservice.config.jwt.JwtAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 @Configuration
@@ -14,7 +22,29 @@ import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 @Import(SecurityProblemSupport.class)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtFilter filter;
     private final SecurityProblemSupport problemSupport;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws
+            Exception {
+        return super.authenticationManagerBean();
+    }
+
 
     public SecurityConfiguration(SecurityProblemSupport problemSupport) {
         this.problemSupport = problemSupport;
